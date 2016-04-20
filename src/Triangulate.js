8e0bccb6aa97ea7828177666511e1d463b89fa8a
Jan Marsch <jama@jama-mac.fritz.box>
2016-04-20 15:00:36 +0200

    missing isClockwise() added

--- src/Triangulate.js
+++ src/Triangulate.js
@@ -96,6 +96,12 @@ var Triangulate = {};
     }
   };
 
+  function isClockWise(ring) {
+    return 0 < ring.reduce(function(a, b, c, d) {
+      return a + ((c < d.length - 1) ? (d[c+1][0] - b[0]) * (d[c+1][1] + b[1]) : 0);
+    }, 0);
+  }
+
   function process(res, id, properties, geom, position, color) {
     var geometry = transform(geom, position),
       bbox = getBBox(geometry[0]),
